95d7e3686fefd5bc0fb2096ea3eb3640ca3e79d4
AdelinoJnr <adelinojunior96@gmail.com>
2021-10-20 11:24:04 -0300

    feat: model users

--- src/models/users.js
+++ src/models/users.js
@@ -1,1 +1,27 @@
 const getConnection = require('./connection');
+const { ObjectId } = require('mongodb');
+
+const create = async (data) => {
+  const db = await getConnection();
+  const user = await db.collection('users').insertOne({ ...data });
+  return { id: user.insertedId, ...data };
+};
+
+const getAll = async () => {
+  const db = await getConnection();
+  const users = await db.collection('users').find().toArray();
+  return users;
+};
+
+const getById = async (id) => {
+  if (!ObjectId.isValid(id)) return null;
+  const db = await getConnection();
+  const user = await db.collection('users').findOne({ _id: ObjectId(id) });
+  return user;
+};
+
+module.exports = {
+  create,
+  getAll,
+  getById,
+};
