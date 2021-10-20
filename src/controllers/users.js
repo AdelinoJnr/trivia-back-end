f3bccb7994e8db027c3c882bcbc6d42a5119c981
AdelinoJnr <adelinojunior96@gmail.com>
2021-10-20 11:33:11 -0300

    feat: controller users

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,1 +1,28 @@
 const Users = require('../services/users');
+
+const create = async (req, res) => {
+  const { status, data, message } = await Users.create(req.body);
+  if (message) return res.status(status).json({ message });
+
+  res.status(status).json(data);
+};
+
+const getAll = async (_req, res) => {
+  const { status, data } = await Users.getAll();
+
+  res.status(status).json(data);
+};
+
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const { status, data, message } = await Users.getById(id);
+  if (message) return res.status(status).json({ message });
+
+  res.status(status).json(data);
+};
+
+module.exports = {
+  create,
+  getAll,
+  getById,
+};
